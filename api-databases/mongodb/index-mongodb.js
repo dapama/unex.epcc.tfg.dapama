22c4c926bfcc5408cbdc4f4ce23675b3b397bbc9
dapama <dpachecomarroyo@gmail.com>
2019-01-13 11:22:34 +0100

    References to node js deleted.

--- api-databases/mongodb/index-mongodb.js
+++ api-databases/mongodb/index-mongodb.js
@@ -1,30 +1,17 @@
 const MongoClient = require('mongodb').MongoClient
 const config = require('./config/database');
 
+var jsonData = require('../../ftp-data/json-files/quikscat-l2b12/001.json');
+
+var data = JSON.stringify(jsonData);
+console.log(data)
 
 ( async function() {
-
   const client = await MongoClient.connect( config.url, { useNewUrlParser: true } );
   const db = client.db('unex_epcc_tfg');
-  const collection = db.collection('l2b12');
-
-  console.log('connected');
-
-  // collection.deleteMany({})
-  // .then(function() {
-  //     const promises = products.map((product) => {
-  //         console.log(product.name);
-  //         ticketTotal += product.cost();
-  //         return collection.insertOne(product);
-  //     });
-
-  //     return Promise.all(promises);
-  // }).then(function(results) {
-  //     for (let result of results) {
-  //         console.log(result.result);
-  //     }
-  //     console.log("Ticket Total: " + ticketTotal);
-  //     client.close();
-  // });
+  console.log('Connected to MongoDB');
+  const collection = db.collection('l2b12_data');
+  
+  collection.insertMany(data);
 
 })();
