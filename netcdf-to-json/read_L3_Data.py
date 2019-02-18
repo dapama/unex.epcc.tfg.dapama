45a1130acbc6dafd6eb77d6a1ae4b98be4cb4226
David Pacheco <dpachecomarroyo@gmail.com>
2019-02-18 08:13:11 +0100

    First steps updating netcdf to json process

--- netcdf-to-json/read_L3_Data.py
+++ netcdf-to-json/read_L3_Data.py
@@ -3,6 +3,7 @@ from functools import reduce
 from read_nc import read_vars
 from netCDF4 import Dataset
 
+
 format_type = 'NETCDF3_CLASSIC'
 
 # WindSAT
@@ -14,6 +15,16 @@ def extract_data_list_l3( files_list ):
                 l3_net_cdf_to_json( file_i[:-3] )
         f_in.close()
         os.remove( file_i[:-3] )
+
+
+def extract_data_list_l2( files_list ):
+    for file_i in files_list:
+        with gzip.open(file_i, 'rb') as f_in:
+            with open(file_i[:-3], 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+                l2_net_cdf_to_json(file_i[:-3])
+        f_in.close()
+        os.remove(file_i[:-3])
 
 
 def l3_net_cdf_to_json( file_nc ):
@@ -30,7 +41,30 @@ def l3_net_cdf_to_json( file_nc ):
             sea_surface_temperature = var_data_list[3]
 
             # flag = True
+            with open( file_nc + '.json', 'w' ) as outfile:
+                outfile.write( "{\n\t" + '"'"data"'"' + " : [\n" )
+                # for i in range(0, len(latitudes)):
+                
+                for i in range(0, len( times )):
+                    t = time_to_string( times[i] )
 
+                    for j in range(0, len(latitudes)):
+                        la = "{:0.2f}".format(latitudes[j])
+                        for k in range(0, len(longitudes)):
+                            lo = "{:0.2f}".format(longitudes[k])
+                            ws = string_variable(wind_speed[i][j][k])
+                            # rr = string_variable(rain_rate[i][j][k])
+                            sst = string_variable(sea_surface_temperature[i][j][k])
+
+                            outfile.write("\t\t{" + '"'"time"'"' + ": " + t + ", " '"'"lat"'"' + ": " + la +
+                                          ", " '"'"lon"'"' + ": " + lo + ", " + '"'"wind_speed"'"' ": " + ws +
+                                          ", " + '"'"surface_temperature"'"' ": " + sst + "},\n")
+
+                outfile.seek(-3, os.SEEK_END)
+                outfile.truncate()
+
+                outfile.write("\n\t]\n}")
+            outfile.close()
 
         nc.close()
     except IOError:
@@ -78,7 +112,3 @@ def time_to_string(time):
     day = datetime.datetime.fromtimestamp(time).strftime('%j')
     date = str(int(year) * 1000 + int(day))
     return date
-
-
-def retrieve_l3_data( var_data_list ):
-    print 'asd'
